86c7243e6a7d70dbcba4c71f1ba694b49bb3c5c8
Hendy Rodrigues F. Silva <mr.syslogg@gmail.com>
2018-05-15 16:25:11 -0300

    Added EnableConfigServer and adjusted properties

--- src/main/java/br/com/hendyrodrigues/ConfigServer/ConfigServerApplication.java
+++ src/main/java/br/com/hendyrodrigues/ConfigServer/ConfigServerApplication.java
@@ -2,6 +2,7 @@ package br.com.hendyrodrigues.ConfigServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
