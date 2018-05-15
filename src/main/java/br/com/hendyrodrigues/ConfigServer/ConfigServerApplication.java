fb83f766f619d2436af4d7811302fbf44dfdee23
Hendy Rodrigues F. Silva <mr.syslogg@gmail.com>
2018-05-15 16:08:39 -0300

    Initial commit

---
+++ src/main/java/br/com/hendyrodrigues/ConfigServer/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.hendyrodrigues.ConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
